4ef57fd2c803984b86d72d5535b76c6076fdd7fe
eguo <eddy.guo@itchinasource.com>
2014-09-27 21:14:42 -0400

    noble debug code.

--- 1.js
+++ 1.js
@@ -1,1 +1,14 @@
-//
+var noble = require('noble');
+
+noble.on('stateChange', function(state) {
+  console.log("state: " + state);
+  if (state === 'poweredOn') {
+    noble.startScanning([], true);
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on('discover', function(peripheral) {
+  console.log(peripheral)
+});
